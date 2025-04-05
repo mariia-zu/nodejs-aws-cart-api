d5d9b8663fe64c847c9171ad32c095ff45b7a5b0
Mariia Zubkova <mariia_zubkova@epam.com>
2025-04-05 23:54:32 +0200

    task8: add lambda

--- cdk-service/lib/cdk-service-stack.ts
+++ cdk-service/lib/cdk-service-stack.ts
@@ -1,16 +1,38 @@
 import * as cdk from 'aws-cdk-lib';
+import { Duration } from 'aws-cdk-lib';
+import {
+  Code,
+  Function,
+  FunctionUrlAuthType,
+  HttpMethod,
+  Runtime,
+} from 'aws-cdk-lib/aws-lambda';
 import { Construct } from 'constructs';
-// import * as sqs from 'aws-cdk-lib/aws-sqs';
+import { config } from 'dotenv';
+
+config();
 
 export class CdkServiceStack extends cdk.Stack {
   constructor(scope: Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
-    // The code that defines your stack goes here
+    const environment = {};
 
-    // example resource
-    // const queue = new sqs.Queue(this, 'CdkServiceQueue', {
-    //   visibilityTimeout: cdk.Duration.seconds(300)
-    // });
+    const cartApiLambda = new Function(this, 'cartApiLambdaFunction', {
+      runtime: Runtime.NODEJS_22_X,
+      code: Code.fromAsset('../dist/'),
+      handler: 'lambda.handler',
+      timeout: Duration.seconds(5),
+      environment,
+    });
+
+    cartApiLambda.addFunctionUrl({
+      authType: FunctionUrlAuthType.NONE,
+      cors: {
+        allowedOrigins: ['*'],
+        allowedMethods: [HttpMethod.ALL],
+        allowedHeaders: ['*'],
+      },
+    });
   }
 }
